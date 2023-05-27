3c2ef10e25eff0ff64e1a81ee30bc9055f9b7436
Stiv0r <Stiv0r@icloud.com>
2023-05-26 22:33:15 -0800

    proekt5

---
+++ proekt5/Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace proekt5
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(vod.Text);
+
+            double sum = 1; 
+            int factorial = 1; 
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+                sum += 1.0 / factorial;
+            }
+
+            otv.Text=("Сумма выражения: " + sum);
+        }
+    }
+}
---
+++ proekt5/Window2.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace proekt5
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(vod.Text);
+            int k = int.Parse(vod2.Text);
+
+            double sum = 0; 
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += Math.Pow(i, k);
+            }
+
+            otv.Text=("Сумма выражения: " + sum);
+        }
+    }
+}
